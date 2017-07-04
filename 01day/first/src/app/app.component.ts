92beeda40ee8b11be1eeb2acb4ae1ba1c79eb570
junreturn1982 <ngochoang071982@gmail.com>
2017-07-04 15:35:27 +0700

    first app

---
+++ 01day/first/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>
+      Welcome to {{title}}!!
+    </h1>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app first: Hello Hoang';
+}
