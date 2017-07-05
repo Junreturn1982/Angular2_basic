44e143b6cd1f820101ef6dc472c578c50362314a
junreturn1982 <ngochoang071982@gmail.com>
2017-07-05 11:57:55 +0700

    ngif

--- 01day/first/src/app/container/contact-list/contact-list.component.ts
+++ 01day/first/src/app/container/contact-list/contact-list.component.ts
@@ -2,18 +2,10 @@ import { Component } from '@angular/core';
 // view
 @Component({
     selector: 'hoang-contact-list',
-    template: `
-        <h2>{{ message }}</h2>
-        <button (click)="doClick()">Click me</button>
-    `
+    templateUrl: './contact-list.component.html'
 })
 // controller
 export class ContactListComponent {
     message: string = "Xin chao, ContactListComponent";
-    doClick() {
-        this.message = "Button clicked";
-        setTimeout(() => {
-            this.message = "Button done!";
-        }, 1000);
-    }
+    printable = true;
 }
