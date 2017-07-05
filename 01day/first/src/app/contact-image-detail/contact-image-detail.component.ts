3f1f03940fe35e2791ea89cde55f206af338072d
junreturn1982 <ngochoang071982@gmail.com>
2017-07-05 10:00:29 +0700

    ng genarate component

---
+++ 01day/first/src/app/contact-image-detail/contact-image-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hoang-contact-image-detail',
+  templateUrl: './contact-image-detail.component.html',
+  styleUrls: ['./contact-image-detail.component.css']
+})
+export class ContactImageDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
