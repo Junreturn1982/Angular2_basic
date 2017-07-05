1180111a526185c7746a4f18a8bbfeb9a80d393b
junreturn1982 <ngochoang071982@gmail.com>
2017-07-05 11:06:51 +0700

    event binding

--- 01day/first/src/app/container/contact-list/contact-list.component.ts
+++ 01day/first/src/app/container/contact-list/contact-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+// view
 @Component({
     selector: 'hoang-contact-list',
     template: `
         <h2>{{ message }}</h2>
+        <button (click)="doClick()">Click me</button>
     `
 })
-
+// controller
 export class ContactListComponent {
     message: string = "Xin chao, ContactListComponent";
+    doClick() {
+        this.message = "Button clicked";
+        setTimeout(() => {
+            this.message = "Button done!";
+        }, 1000);
+    }
 }
