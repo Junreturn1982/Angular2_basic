6a18ec7e1769d907a9fe089ee92cab3fa88402ef
junreturn1982 <ngochoang071982@gmail.com>
2017-07-11 09:16:31 +0700

    template variable

---
+++ 02demo/client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `
+  //   <h1>
+  //     Welcome to {{title}}!!
+  //   </h1>
+  // `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app first: Hello Hoang';
+  
+  sayHello(name: string) {
+    console.log(name);
+  }  
+
+  onSubmit() {
+    
+  }
+}
---
+++ 02demo/client/src/app/container/switches/switches.component.ts
@@ -0,0 +1,31 @@
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  Output, 
+  EventEmitter 
+} from '@angular/core';
+
+@Component({
+  selector: 'hoang-switches',
+  templateUrl: './switches.component.html',
+  styleUrls: ['./switches.component.scss']
+})
+export class SwitchesComponent implements OnInit {
+  @Input() checked: Boolean = false;
+  @Output('checkedChange') change: EventEmitter<boolean> 
+                            = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggle(): void {
+    this.checked = !this.checked;
+  }
+  // passing data output component
+  emitChangeValue(event): void {
+    // event.stopPropagation();
+    this.change.emit(event.target.checked);
+  }
+}
