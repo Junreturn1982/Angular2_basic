0feb3c607de0c1ab80a3f3ac61db13d29168a9b2
junreturn1982 <ngochoang071982@gmail.com>
2017-07-14 13:25:14 +0700

    ContentChild

--- 02demo/client/src/app/collapse-group/collapse-group.component.ts
+++ 02demo/client/src/app/collapse-group/collapse-group.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentInit } from '@angular/core';
+import { CollapseComponent } from "app/collapse/collapse.component";
 
 @Component({
   selector: 'tp-collapse-group',
   templateUrl: './collapse-group.component.html',
   styleUrls: ['./collapse-group.component.scss']
 })
-export class CollapseGroupComponent implements OnInit {
-
+export class CollapseGroupComponent implements OnInit, AfterContentInit {
+  @ContentChild(CollapseComponent) collapse: CollapseComponent;
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+    console.log(this.collapse);
+  }
 }
